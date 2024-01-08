381193a190a3618d54610a2889802dda2f42d7d3
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-01-08 13:15:27 +0800

    fix_2

--- app/component/SidebarC.tsx
+++ app/component/SidebarC.tsx
@@ -59,14 +59,13 @@ const SidebarC = () => {
           {/* Bab 1 */}
           <ul className="pt-2">
             <>
-              <Link href="/bab2_1">
+              <Link href="/bab1_1">
                 <li
                   className={`${
                     open1 && "w-[275px]"
                   } text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white hover:text-teal-200 rounded-md 
-                ${pathname === "/bab2_1" && "bg-teal-500"}
-                ${pathname === "/bab2_2" && "bg-teal-500"}
-                ${pathname === "/bab2_kuis" && "bg-teal-500"}`}
+                ${pathname === "/bab1_1" && "bg-teal-500"}
+                ${pathname === "/bab1_kuis" && "bg-teal-500"}`}
                   onClick={() => setSubmenutOpen1(!submenuOpen1)}
                 >
                   <span className="text-2xl block float-left">

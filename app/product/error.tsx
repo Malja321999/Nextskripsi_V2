54da3d7181cfc2beec96bb91a565d34d16c387f0
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-01-11 22:32:01 +0800

    fix_error_page

--- app/product/error.tsx
+++ app/product/error.tsx
@@ -13,13 +13,13 @@ export default function Error({
     /*   reset(); */
   }, [error]);
   return (
-    <div className="flex justify-center items-center place-items-center bg-sky-500 text-center rounded-md h-screen mt-10 p-20">
-      <h1 className="bg-rose-500 text-center text-9xl p-5 rounded-md w-fit">
+    <div className="flex flex-col justify-center items-center bg-rose-500 text-center rounded-md h-screen mt-10 p-20 gap-5">
+      <h1 className=" text-center text-9xl p-5 rounded-md w-fit">
         Something went wrong!
       </h1>
       <button
         onClick={() => reset()}
-        className="bg-sky-500 p-5 rounded-md w-fit text-center hover:bg-teal-500"
+        className="bg-emerald-500 p-5 rounded-full border border-gray-500 w-fit text-center hover:bg-emerald-400 text-5xl"
       >
         Try Again
       </button>

1b55a534c06fe1e432ad78b739fe1524564f5e69
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-01-13 01:12:05 +0800

    fix

--- app/api/user/route.ts
+++
@@ -1,6 +0,0 @@
-/* import { NextResponse, NextRequest } from "next/server";
-import { signUp } from "@/app/lib/firebase/service";
-
-export default function POST() {
- return NextRequest.json({ status:"200", message: "success" });
-} */

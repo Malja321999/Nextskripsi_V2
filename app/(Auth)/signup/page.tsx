ff7df3bccbcf88b8e590206e9efc623e21d80b84
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-01-13 12:03:39 +0800

    13-1-24

--- app/(Auth)/signup/page.tsx
+++ app/(Auth)/signup/page.tsx
@@ -6,11 +6,41 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const SignUp = () => {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    console.log(res.status);
+    if (res.status === 200) {
+      e.target.reset();
+      setIsLoading(false);
+      push("/signin");
+    } else {
+      console.log(res);
+      setError("Email Already Exists");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="bg-white dark:bg-black min-h-screen flex items-center justify-center text-white mx-auto">
-      <div className="h-screen w-auto flex justify-center items-center">
+      <div className="h-screen w-auto flex justify-center items-center flex-col">
+        {error !== "" && (
+          <div className="text-red-600 text-5xl font-bold mb-3">{error}</div>
+        )}
         <div className="bg-white w-[60vh] shadow-md border border-gray-500 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6" action="#">
+          <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
             <h2 className="dark:text-white mb-5 text-center text-2xl font-bold leading-9 tracking-tight">
               <div className="w-10 h-10 flex justify-center items-center mx-auto ">
                 <Image
@@ -71,19 +101,12 @@ const SignUp = () => {
                 required
               />
             </div>
-            <div className="flex items-start">
-              <Link
-                href="#"
-                className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500"
-              >
-                Lost Password?
-              </Link>
-            </div>
             <button
+              disabled={isLoading}
               type="submit"
               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Sign up your account
+              {isLoading ? "Loading..." : "Sign up account"}
             </button>
             <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
               Have an account registered?{" "}
--- app/lib/firebase/service.tsx
+++ app/lib/firebase/service.tsx
@@ -1,22 +1,26 @@
 import {
+  addDoc,
   collection,
   doc,
   getDoc,
   getDocs,
   getFirestore,
+  query,
+  where,
 } from "firebase/firestore";
 import app from "./init";
+import bcrypt from "bcryptjs";
 
 const firestore = getFirestore(app);
 
+/* Mengambil Data Dari Firebase */
+
 export async function retrieveData(collectionName: string) {
   const snapshot = await getDocs(collection(firestore, collectionName));
-  const data = snapshot.docs.map((doc) => {
-    return {
-      id: doc.id,
-      ...doc.data(),
-    };
-  });
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
   return data;
 }
 
@@ -24,4 +28,35 @@ export async function retrieveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(firestore, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+/* Menghubungkan Signin Dan Signup dengan Firebase */
+
+export async function register(data: {
+  fullname: string;
+  email: string;
+  password: string;
+  role?: string;
+}) {
+  const q = query(
+    collection(firestore, "users"),
+    where("email", "==", data.email)
+  );
+  const shapshot = await getDocs(q);
+  const users = shapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  if (users.length > 0) {
+    return { status: false, statusCode: 400, message: "Email already exist" };
+  } else {
+    data.role = "admin";
+    data.password = await bcrypt.hash(data.password, 10);
+    try {
+      await addDoc(collection(firestore, "users"), data);
+      return { status: true, statusCode: 200, message: "Register Success" };
+    } catch (error) {
+      return { status: false, statusCode: 400, message: "Register Failed" };
+    }
+  }
 }

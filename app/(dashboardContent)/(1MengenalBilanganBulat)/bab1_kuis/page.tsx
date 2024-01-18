12d83954ca42ea5a9fdd35fd92a160eeb415c24e
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-01-18 17:10:09 +0800

    Update_Bab1_Kuis

--- app/(dashboardContent)/(1MengenalBilanganBulat)/bab1_kuis/page.tsx
+++ app/(dashboardContent)/(1MengenalBilanganBulat)/bab1_kuis/page.tsx
@@ -1,16 +1,11 @@
 import ButttonpnUP from "@/app/component/ButttonpnUP";
 import Pra_Tujuan from "@/app/component/Pra_Tujuan";
-import Link from "next/link";
-import React from "react";
+import Bab1_Kuis from "@/app/kuis/Bab1_Kuis";
 
 const page = () => {
   return (
-    <div className="bg-rose-700 h-[37.1rem] w-[88.8rem] mt-[5rem] mx-2 mb-5 p-10 flex flex-col relative rounded-md left-[-90.9rem] text-3xl text-white">
-      Bab1 Kuis
-      <Pra_Tujuan t={"Mengenal Bilangan Bulat"} />
-      <br />
-      <br />
-      <ButttonpnUP p={"/bab1_1"} n={"/bab2_1"} />
+    <div className="bg-rose-700 h-[37.1rem] w-[88.8rem] mt-[5rem] mx-2 mb-5 p-5 flex flex-col relative rounded-md left-[-90.9rem] text-3xl text-white">
+      <Bab1_Kuis />
     </div>
   );
 };
---
+++ app/kuis/QuizCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { useState } from "react";
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+
+const QuizCard = ({
+  question,
+  options,
+  correctAnswer,
+  onNext,
+  currentQuestion,
+  setCurrentQuestion,
+  lastQuestion,
+}: any) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleAnswerSelect = (option: any) => {
+    setSelectedAnswer(option);
+  };
+
+  const handleNextClick = () => {
+    onNext(selectedAnswer === correctAnswer);
+    setSelectedAnswer(null);
+  };
+
+  return (
+    <div className="bg-teal-500 w-[60rem] h-fit rounded-md p-5 shadow-md text-5xl">
+      <div>
+        <h1 className="font-bold bg-sky-700 rounded-md border-4 border-white text-center p-5">
+          Kuis {currentQuestion + 1}
+        </h1>
+        <ul className="bg-white rounded-md border-4 border-sky-400 p-10 mt-5 text-sky-500">
+          <p>{question}</p>
+
+          {options.map((option: any) => {
+            return (
+              <li className="px-5" key={option}>
+                <input
+                  className="mr-2 h-6 w-6"
+                  type="radio"
+                  key={option}
+                  checked={selectedAnswer === option}
+                  onClick={() => handleAnswerSelect(option)}
+                />
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="flex justify-between mt-5 text-base w-50 gap-10">
+          {currentQuestion > 0 ? (
+            <button
+              onClick={() => setCurrentQuestion(currentQuestion - 1)}
+              className="bg-sky-700 rounded-md shadow-md cursor-pointer text-center font-bold w-fit p-2 flex justify-center items-center"
+            >
+              <span className="-left-1 relative">
+                <MdNavigateBefore size={40} />
+              </span>
+            </button>
+          ) : (
+            <div></div>
+          )}
+          {currentQuestion < lastQuestion ? (
+            <button
+              onClick={handleNextClick}
+              className="bg-sky-700 rounded-md shadow-md cursor-pointer text-center font-bold w-fit p-2 flex justify-center items-center"
+            >
+              <span className=" relative">
+                <MdNavigateNext size={40} />
+              </span>
+            </button>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuizCard;

f4749615f749a534ad97487cffcdf49a7b18bb40
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-05-20 00:01:42 +0800

    fix_pagetation_number_bg_select

--- app/no-ssr_data_murid/NOSSR.tsx
+++ app/no-ssr_data_murid/NOSSR.tsx
@@ -211,7 +211,9 @@ function NOSSR() {
                 {numbers.map((i) => (
                   <button
                     key={i}
-                    className="relative z-10 inline-flex items-center bg-white px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0"
+                    className={`${
+                      currentPage === i ? "bg-blue-500" : "bg-white"
+                    } relative z-10 inline-flex items-center  px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0"`}
                     onClick={() => changePage(i)}
                   >
                     {i}
